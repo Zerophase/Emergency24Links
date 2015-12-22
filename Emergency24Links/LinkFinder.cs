a08dcde9ca191fe876b4a5ae1ae4d6e2302b46a0
zerophase <mikelojkvic@gmail.com>
2015-12-22 07:12:52 -0600

    Fix double listing bug

---
+++ Emergency24Links/LinkFinder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Emergency24Links
+{
+	public static class LinkFinder
+	{
+		private static MatchCollection topMatch;  
+
+		private const string CP = "/cp/";
+
+		private static string newLine = Environment.NewLine;
+
+		public static List<string> Find(string file)
+		{
+			Console.WriteLine("Finding Links...");
+
+			topMatch = Regex.Matches(file, @"(<body.*?>.*?</body>)",
+				RegexOptions.Singleline);
+
+			List<string> links = new List<string> ();
+
+			var cpList = extractLink (
+				@"href=""(https\://www\.emergency24\.com/cp/)?(/cp/)?(([A-Za-z0-9\-_]+(\.aspx)?|(#.*?))|(val/)|(subresp/))""", 3);
+
+			StringBuilder stringBuilder = new StringBuilder (cpList.Count);
+			foreach (var link in cpList)
+			{
+				stringBuilder.Append (CP);
+				stringBuilder.AppendLine(link);
+			}
+			links.AddRange (stringBuilder.ToString ().Split(newLine.ToCharArray(), 
+				StringSplitOptions.RemoveEmptyEntries));
+
+			var httpList = extractLink (@"href=""(?!https://www.emergency24.com/cp/subresp/)(https?.*?)""", 1);
+			links.AddRange (httpList);
+
+			return links;
+		}
+
+		public static List<string> extractLink(string regex, int group)
+		{
+			List<string> strings = new List<string> ();
+			foreach (Match match in topMatch)
+			{
+				MatchCollection m = Regex.Matches (match.Value, 
+					regex, RegexOptions.Singleline);
+				for (int i = 0; i < m.Count; i++) 
+				{
+					strings.Add (m[i].Groups[group].Value);
+				}
+			}
+
+			return strings;
+		}
+	}
+}
+
+
---
+++ Emergency24Links/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Linq;
+
+namespace Emergency24Links
+{
+	class MainClass
+	{
+		const string EMERGENCY_24 = "http://www.emergency24.com";
+		const string FILE_NAME = "links.txt";
+
+		public static void Main (string[] args)
+		{
+			WebClient w = new WebClient ();
+			string s = w.DownloadString (EMERGENCY_24);
+
+
+			var links = LinkFinder.Find(s).Distinct().ToList ();
+			Console.WriteLine ("Alphabetizing Links...");
+			for (int i = 0; i < links.Count; i++) 
+			{
+				if (links[i].Contains ("/cp/"))
+					links[i] = links[i].Insert (0, EMERGENCY_24);
+			}
+
+			links.Sort ();
+
+			using (StreamWriter writer = new StreamWriter (FILE_NAME)) 
+			{
+				Console.WriteLine ("Writing links to file...");
+				foreach (var link in links) 
+				{
+					writer.WriteLine (link);
+				}
+
+				Console.WriteLine ("Path to text file: {0}.", Path.GetFullPath(FILE_NAME));
+			}
+		}
+	}
+}
